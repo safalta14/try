23fdf874a641b1e763c9a424a7deeefa6a112a05
unknown <shahsafalta0@gmai.com>
2025-06-11 20:01:26 +0545

    settingsssss

--- newstart/settings.py
+++ newstart/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@q!+*&!f=6t411%$h6%-@3r6^o-=fh^81$kjur0_82530y!4c^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['chiya-mine.onrender.com', 'localhost', '127.0.0.1']
 
