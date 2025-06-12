d73655edd5a16e98e40c92ace3f0cd433718783b
unknown <shahsafalta0@gmai.com>
2025-06-12 20:12:56 +0545

    static change

--- newstart/settings.py
+++ newstart/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'STATIC']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # WhiteNoise still required for static file serving
